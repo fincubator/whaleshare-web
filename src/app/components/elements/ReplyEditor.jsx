aa36c59cbee73064a9878ed8069cf69210a243b1
Tuan Pham Anh <baabeetaa@gmail.com>
2018-05-05 05:32:03 +0700

    hotfix default is 100% powerup

--- src/app/components/elements/ReplyEditor.jsx
+++ src/app/components/elements/ReplyEditor.jsx
@@ -631,11 +631,11 @@ export default (formId) => connect(
                             max_accepted_payout: '0.000 SBD',
                         }
                         break;
-                    case '100%': // 100% steem power payout
-                        __config.comment_options = {
-                            percent_steem_dollars: 0, // 10000 === 100% (of 50%)
-                        }
-                        break;
+                    // case '100%': // 100% steem power payout
+                    //     __config.comment_options = {
+                    //         percent_steem_dollars: 0, // 10000 === 100% (of 50%)
+                    //     }
+                    //     break;
                     default: // 50% steem power, 50% sd+steem
                 }
             }
