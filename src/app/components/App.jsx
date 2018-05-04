9164c11370e3560afbee6fa01ff53f4a99d3db6d
Vitaly Kolesnik <vit.kolesnik@gmail.com>
2018-05-04 22:04:15 +0200

    Added and styled a testnet notice

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -34,7 +34,7 @@ const pageRequiresEntropy = (path) => {
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {open: null, showCallout: true, showBanner: true, expandCallout: false};
+        this.state = {open: null, showCallout: true, showBanner: true, showNotice: true, expandCallout: false};
         this.toggleOffCanvasMenu = this.toggleOffCanvasMenu.bind(this);
         this.signUp = this.signUp.bind(this);
         this.learnMore = this.learnMore.bind(this);
@@ -87,6 +87,7 @@ class App extends React.Component {
             p.flash !== n.flash ||
             this.state.open !== nextState.open ||
             this.state.showBanner !== nextState.showBanner ||
+            this.state.showNotice !== nextState.showNotice ||
             this.state.showCallout !== nextState.showCallout ||
             p.nightmodeEnabled !== n.nightmodeEnabled
         );
@@ -172,6 +173,20 @@ class App extends React.Component {
             </div>;
         }
 
+        let sitewide_notice = null;
+        if (this.state.showNotice) { 
+            sitewide_notice = (
+              <div className="sitewideNotice">
+                <CloseButton onClick={() => this.setState({showNotice: false})} />
+                <div className="text-center">
+                    <p><strong>Please note:</strong> This is a TestNet. TestNet WLS / WHALESTAKE tokens have no value and will be reset before launch.
+                    <a className="button hollow uppercase" href="/whaleshares/@powerpics/important-information-about-the-whaleshares-beta" target="_blank" rel="noopener noreferrer" onClick={this.learnMore}> <strong>{tt('navigation.learn_more')}</strong></a>
+                    </p>
+                </div>
+              </div>
+            );
+        }
+
         let welcome_screen = null;
         if (ip && new_visitor && this.state.showBanner) { 
             welcome_screen = (
@@ -233,6 +248,7 @@ class App extends React.Component {
             {miniHeader ? headerHidden ? null : <MiniHeader /> : <Header toggleOffCanvasMenu={this.toggleOffCanvasMenu} menuOpen={this.state.open} />}
             <div className="App__content">
                 {welcome_screen}
+                {sitewide_notice}
                 {callout}
                 {children}
                 {lp ? <LpFooter /> : null}
