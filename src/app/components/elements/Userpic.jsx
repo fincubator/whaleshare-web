ab2fe7f5e12e57328882d6ed7afabdd65f863e21
Tuan Pham Anh <baabeetaa@gmail.com>
2018-11-09 03:10:19 +0700

    fix image profile

--- src/app/components/elements/Userpic.jsx
+++ src/app/components/elements/Userpic.jsx
@@ -7,8 +7,6 @@ export const SIZE_SMALL = 'small';
 export const SIZE_MED = 'medium';
 export const SIZE_LARGE = 'large';
 
-const sizeList = [SIZE_SMALL, SIZE_MED, SIZE_LARGE]
-
 export const avatarSize = {
   small: SIZE_SMALL,
   medium: SIZE_MED,
@@ -17,34 +15,26 @@ export const avatarSize = {
 
 class Userpic extends Component {
 
-  shouldComponentUpdate = shouldComponentUpdate(this, 'Userpic')
+  shouldComponentUpdate = shouldComponentUpdate(this, 'Userpic');
 
   render() {
-    const {json_metadata, account} = this.props;
+    const {account, size} = this.props;
 
     if ((typeof(account) === "undefined") || (account === null) || (account === '')) {
       return null;
     }
 
-    let profileImageUrl = `/images/user.png`;
-
-    // try to extract image url from users metaData
-    try {
-      const metadata = JSON.parse(json_metadata);
-
-      if (metadata.profile.profile_image) {
-        if (/^(https?:)\/\//.test(metadata.profile.profile_image)) {
-          // hack to get profile images to display. This doesn't work if there is no metadata
-          profileImageUrl = `${imageProxy()}32x32/${metadata.profile.profile_image}`;
-        }
-      }
-    } catch (error) {
-      // just use the convention
-      profileImageUrl = `https://${$STM_Config.site_domain}/profileimage/${account}`;
+    let img_size = "64x64";
+    if (size === SIZE_SMALL) {
+      img_size = "32x32";
+    } else if (size === SIZE_MED) {
+      img_size = "48x48";
+    } else if (size === SIZE_LARGE) {
+      img_size = "96x96";
     }
 
-    return (<div className="Userpic" style={{backgroundImage: `url(${profileImageUrl})`}}/>)
-
+    const profile_image_url = `${imageProxy()}profileimage/${account}/${img_size}`;
+    return (<div className="Userpic" style={{backgroundImage: `url(${profile_image_url})`}}/>)
   }
 }
 
@@ -57,7 +47,6 @@ export default connect(
     const {account, hideIfDefault} = ownProps
     return {
       account,
-      json_metadata: state.global.getIn(['accounts', account, 'json_metadata']),
       hideIfDefault,
     }
   }
